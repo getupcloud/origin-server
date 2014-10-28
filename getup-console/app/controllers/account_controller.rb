050cac449c5be3406dcbc6f443c464e0619d0c41
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2014-10-28 00:08:01 -0200

    My Account show credits

--- getup-console/app/controllers/account_controller.rb
+++ getup-console/app/controllers/account_controller.rb
@@ -2,8 +2,8 @@ class AccountController < ConsoleController
   include Console::UserManagerHelper
 
   def show
-    plan = user_manager_account_plan.content
-    @payment = plan[:payment]
+    @plan = user_manager_account_plan.content
+    @payment = @plan[:payment]
     @capabilities = user_capabilities :refresh => true
   end
 end
