5c305b0ed4c97c0c72c1c084b651e68a7134c2bc
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2015-06-27 23:13:28 -0300

    Fix po load path

--- console/config/initializers/i18n.rb
+++ console/config/initializers/i18n.rb
@@ -1,7 +1,6 @@
 require 'gettext'
 include GetText
 
-I18n.enforce_available_locales = true
 I18n::Backend::Simple.include(I18n::Backend::Gettext)
 
 GetText::LocalePath.add_default_rule File.absolute_path(File.dirname(__FILE__) + '/../locales/%{lang}/LC_MESSAGES/%{name}.mo')
