eddf6c35f062d1f5944dc8a02f08db58f37c28b1
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2015-08-03 11:03:49 -0300

    Desablita PayPal

--- console/app/controllers/account_controller.rb
+++ console/app/controllers/account_controller.rb
@@ -6,6 +6,7 @@ class AccountController < ConsoleController
     @user = current_user
     @plan = user_manager_account_plan.content
     @payment = @plan[:payment]
+    @plan[:is_trial] = @plan[:status] == 'te' or @plan[:status] == 'tf'
     @capabilities = user_capabilities :refresh => true
     @getup_credit_url = getup_credit_url
     @has_credits = (@plan[:free_tier].to_f + @plan[:bonus].to_f + @plan[:voucher].to_f + @plan[:credit].to_f) > 0
